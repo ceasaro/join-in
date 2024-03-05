40a77a8673c5b95be64aa4fc0c92c94622960cbe
ceasaro <ceesvw@gmail.com>
2024-03-05 22:28:27 +0100

    Introduced BaseModel

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import Group
 from django.db import models
 
 
-class JoinIn(models.Model):
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class JoinIn(BaseModel):
     group = models.OneToOneField(Group, on_delete=models.CASCADE, primary_key=True)
     slug = models.SlugField(max_length=20, null=False, blank=False)
 
